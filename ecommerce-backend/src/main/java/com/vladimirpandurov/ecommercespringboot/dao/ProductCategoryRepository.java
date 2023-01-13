91bd39aca1e1fbcb5d94c52313cad407993af1ec
panvla <pandvlado@gmail.com>
2023-01-13 14:36:07 +0100

    added product list...

--- ecommerce-backend/src/main/java/com/vladimirpandurov/ecommercespringboot/dao/ProductCategoryRepository.java
+++ ecommerce-backend/src/main/java/com/vladimirpandurov/ecommercespringboot/dao/ProductCategoryRepository.java
@@ -4,8 +4,10 @@ import com.vladimirpandurov.ecommercespringboot.entity.ProductCategory;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 import org.springframework.stereotype.Repository;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
 @RepositoryRestResource(collectionResourceRel = "productCategory", path="product-category")
+@CrossOrigin
 public interface ProductCategoryRepository extends JpaRepository<ProductCategory, Long> {
 
 }
