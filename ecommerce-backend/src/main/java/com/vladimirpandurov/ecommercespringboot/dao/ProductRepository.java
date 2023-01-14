92d2656fa6bb0157b96e8b2037e4f5d0e4938464
panvla <pandvlado@gmail.com>
2023-01-14 20:43:57 +0100

    added side bar filter functionality

--- ecommerce-backend/src/main/java/com/vladimirpandurov/ecommercespringboot/dao/ProductRepository.java
+++ ecommerce-backend/src/main/java/com/vladimirpandurov/ecommercespringboot/dao/ProductRepository.java
@@ -1,12 +1,17 @@
 package com.vladimirpandurov.ecommercespringboot.dao;
 
 import com.vladimirpandurov.ecommercespringboot.entity.Product;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 import org.springframework.web.bind.annotation.CrossOrigin;
 
 @Repository
 @CrossOrigin
 public interface ProductRepository extends JpaRepository<Product, Long> {
+
+    Page<Product> findByCategoryId(@Param("id") Long id, Pageable pageable);
 
 }
